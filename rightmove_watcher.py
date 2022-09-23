45421d3264c83ddf2d9b08478dd485eb795d7451
Matt Lyon <matthewlyon18@gmail.com>
2022-09-23 20:31:02 +0100

    updoots to watcher

--- rightmove_watcher.py
+++ rightmove_watcher.py
@@ -42,9 +42,46 @@ def get_property_list(html_doc: str) -> List[str]:
     soup = BeautifulSoup(html_doc, 'html.parser')
     search_results = soup.find(id='l-searchResults')
     property_elems = search_results.findChildren('div', recursive=False)
-    property_ids = [prop.get('id') for prop in property_elems]
+    properties = [PropertyElement(elem) for elem in property_elems]
 
-    return property_ids
+    return properties
+
+
+class PropertyElement:
+    '''Property Container class'''
+    def __init__(self, elem):
+        self.elem = elem
+        self._parse_id()
+        self._parse_price()
+        self._parse_href()
+        self._parse_location()
+        self._parse_title()
+
+    def __eq__(self, other):
+        return self.id == other.id
+
+    def __str__(self):
+        return f'{self.title}, {self.price_str}\n{self.location}\n{self.href}'
+
+    def _parse_id(self):
+        # pylint: disable=invalid-name
+        self.id = self.elem.get('id').replace('property-', '')
+
+    def _parse_price(self):
+        self.price_str = self.elem.find('span', class_='propertyCard-priceValue').text
+        self.price = self.price_str.replace('pcm', '').replace('£', '').strip()
+
+    def _parse_href(self):
+        rel_link = self.elem.find(
+            'a', class_='propertyCard-priceLink propertyCard-rentalPrice'
+        )['href']
+        self.href = 'https://rightmove.co.uk' + rel_link
+
+    def _parse_location(self):
+        self.location = self.elem.find('address', class_='propertyCard-address').text.strip()
+
+    def _parse_title(self):
+        self.title = self.elem.find('h2', class_='propertyCard-title').text.strip().title()
 
 
 class RightMoveWatcher:
@@ -56,7 +93,7 @@ class RightMoveWatcher:
         self.sess = requests.Session()
         self.interval = interval
         self._property_id_fpath = Path.home().joinpath('.rightmove')
-        self.property_ids = self._get_previous_property_list()
+        self.properties = self._get_previous_property_list()
         self.twilio_creds = get_twilio_creds()
         self.check_depth = 10
         self.max_len = 75
@@ -82,26 +119,26 @@ class RightMoveWatcher:
 
     def _add_properties_to_list(self, new_properties):
         for prop in new_properties:
-            if prop not in self.property_ids:
-                self.property_ids.appendleft(prop)
-                if len(self.property_ids) > self.max_len:
-                    self.property_ids.pop()
+            if prop not in self.properties:
+                self.properties.appendleft(prop)
+                if len(self.properties) > self.max_len:
+                    self.properties.pop()
 
     def _get_search_results(self):
         try:
             res = self.sess.post(self.search_url, params=get_search_params())
             if res.status_code != 200:
                 raise RuntimeError(f'Error in search: {res.status_code}')
-            property_ids = get_property_list(res.text)
+            property_list = get_property_list(res.text)
             self._fail_count = 0
-            self._save_property_list(property_ids)
-            return property_ids
+            self._save_property_list(property_list)
+            return property_list
         except requests.exceptions.ConnectionError:
             self._fail_count += 1
             print('Failed to connect.')
             if self._fail_count > self.fail_thresh:
                 self.send_text('Script failed to connect.')
-            return self.property_ids
+            return self.properties
 
     def send_text(self, msg):
         '''Sends text notification'''
@@ -117,16 +154,16 @@ class RightMoveWatcher:
     def watch(self):
         '''Watches RightMove search page'''
         while True:
-            if len(self.property_ids) == 0:
-                self.property_ids = deque(self._get_search_results())
-                print(f'{len(self.property_ids)} initial properties.')
+            if len(self.properties) == 0:
+                self.properties = deque(self._get_search_results())
+                print(f'{len(self.properties)} initial properties.')
             else:
                 new_properties = self._get_search_results()
                 for idx in range(self.check_depth):
-                    prop_id = new_properties[idx]
-                    if prop_id not in self.property_ids:
-                        print(f'*** New Property: {prop_id} ***')
-                        self.send_text(f'New Property: {prop_id}')
+                    prop = new_properties[idx]
+                    if prop not in self.properties:
+                        print(prop)
+                        self.send_text(str(prop))
                         self._new = True
                 self._add_properties_to_list(new_properties)
                 if self._new:
